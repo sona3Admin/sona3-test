e4717671611fd5fd297e61ae795c911796c186f4
Abrahim Saad <abrahimsaad271@gmail.com>
2024-02-27 16:12:18 +0200

    Cashback Module

--- routes/seller/auth.route.js
+++ routes/seller/auth.route.js
@@ -1,11 +1,16 @@
 const app = require("express").Router();
 const authController = require("../../controllers/seller/auth.controller")
+const sellerController = require("../../controllers/seller/seller.controller")
 const { createSellerValidation, loginValidation } = require("../../validations/seller.validation")
 const validator = require("../../helpers/validation.helper")
-
+const { uploadImagesToMemory } = require("../../helpers/uploader.helper")
+const uploadedFiles = uploadImagesToMemory()
 
 app.post("/register", validator(createSellerValidation), authController.register);
 app.post("/login", validator(loginValidation), authController.login);
+
+app.post("/identity", uploadedFiles.array('image', 2), sellerController.uploadIdentityImages)
+app.delete("/identity", sellerController.deleteIdentityImages)
 
 
 
