50339312384d0406550a187c8b7c662df80a2f74
Hozifa Elgharbawy <Hozifa.elgharbawy12245533@gmail.com>
2024-03-31 16:40:56 +0200

    update sendDataUpdates

--- sockets/notification.socket.js
+++ sockets/notification.socket.js
@@ -9,7 +9,7 @@ exports.notificationSocketHandler = (socket, io, socketId, localeMessages, langu
     socket.on("sendNotificationToGroup", async (dataObject, sendAck) => {
         try {
             console.log("Sending notification");
-            
+
             let validator = await socketValidator(createNotificationValidation, dataObject, language)
             if (!validator.success) return sendAck(validator)
 
@@ -33,4 +33,111 @@ exports.notificationSocketHandler = (socket, io, socketId, localeMessages, langu
 
         }
     })
+
+
+
+    // socket.on("sendStatusUpdates", async (dataObject, sendAck) => {
+    //     try {
+    //         console.log("Sending notification");
+    //         let statusEnumValues = ["pending", "in progress", "delivered", "canceled", "accepted", "rejected", "purchased"]
+    //         if (!statusEnumValues.includes(dataObject.status)) return sendAck({ success: false, code: 500, error: localeMessages.internalServerError })
+
+    //         let bodyTextEn, bodyTextAr, redirectType, redirectId;
+    //         let receiver = {}
+
+    //         let bodyMessages = {
+    //             activate: {
+    //                 en: " is verified",
+    //                 ar: " تم التحقق منه",
+    //             },
+    //             deactivate: {
+    //                 en: " is not verified",
+    //                 ar: " لم يتم التحقق منه"
+    //             },
+    //             changeData: {
+    //                 en: " data has changed",
+    //                 ar: " لقد تغيرت بياناته"
+    //             },
+    //         }
+    //         let sender = {
+    //             _id: socket.handshake.headers['_id'],
+    //             name: "Sona3"
+    //         }
+
+
+    //         if (dataObject.seller) {
+    //             const existingObject = await sellerRepo.find({ _id: dataObject.seller })
+    //             if (!existingObject.success) return sendAck(existingObject)
+    //             receiver = existingObject.result
+    //             redirectId = existingObject.result._id
+    //             redirectType = "seller"
+    //             bodyTextEn = "Your account" + bodyMessages[`${dataObject.action}`].en
+    //             bodyTextAr = "الحساب الخاص بك" + bodyMessages[`${dataObject.action}`].ar
+    //         }
+
+
+    //         if (dataObject.shop) {
+    //             const existingObject = await shopRepo.get({ _id: dataObject.shop })
+    //             if (!existingObject.success) return sendAck(existingObject)
+    //             receiver = existingObject.result.seller
+    //             redirectId = existingObject.result._id
+    //             redirectType = "shop"
+    //             bodyTextEn = "Your shop " + `${existingObject.result.nameEn}` + bodyMessages[`${dataObject.action}`].en
+    //             bodyTextAr = "المتجر الخاصة بك " + `${existingObject.result.nameAr}` + bodyMessages[`${dataObject.action}`].ar
+    //         }
+
+
+    //         if (dataObject.product) {
+    //             const existingObject = await productRepo.get({ _id: dataObject.product })
+    //             if (!existingObject.success) return sendAck(existingObject)
+    //             receiver = existingObject.result.seller
+    //             redirectId = existingObject.result._id
+    //             redirectType = "product"
+    //             bodyTextEn = "Your product " + `${existingObject.result.nameEn}` + bodyMessages[`${dataObject.action}`].en
+    //             bodyTextAr = "المنتج الخاصة بك " + `${existingObject.result.nameAr}` + bodyMessages[`${dataObject.action}`].ar
+    //         }
+
+
+    //         if (dataObject.service) {
+    //             const existingObject = await serviceRepo.get({ _id: dataObject.service })
+    //             if (!existingObject.success) return sendAck(existingObject)
+    //             receiver = existingObject.result.seller
+    //             redirectId = existingObject.result._id
+    //             redirectType = "service"
+    //             bodyTextEn = "Your service " + `${existingObject.result.nameEn}` + bodyMessages[`${dataObject.action}`].en
+    //             bodyTextAr = "الخدمة الخاصة بك " + `${existingObject.result.nameAr}` + bodyMessages[`${dataObject.action}`].ar
+    //         }
+
+
+    //         let notificationObject = {
+    //             admin: sender._id,
+    //             titleEn: `New Notification from ${sender.name}`,
+    //             titleAr: `${sender.name} إشعار جديد من`,
+    //             bodyEn: bodyTextEn,
+    //             bodyAr: bodyTextAr,
+    //             redirectId: redirectId.toString(),
+    //             redirectType: redirectType,
+    //             type: dataObject.action,
+    //             receivers: receiver ? [receiver._id.toString()] : [],
+    //             deviceTokens: receiver ? [receiver.fcmToken] : []
+    //         }
+
+    //         let resultObject = await notificationRepo.create(notificationObject)
+    //         let title = {
+    //             en: resultObject.result.titleEn,
+    //             ar: resultObject.result.titleAr
+    //         }
+    //         let body = {
+    //             en: resultObject.result.bodyEn,
+    //             ar: resultObject.result.bodyAr
+    //         }
+    //         io.to(receiver._id.toString()).emit("newNotification", { success: true, code: 201, result: resultObject.result })
+    //         notificationHelper.sendPushNotification(title, body, resultObject.result.deviceTokens)
+    //         return sendAck(resultObject)
+    //     } catch (err) {
+    //         console.log("err.message", err.message)
+    //         return sendAck({ success: false, code: 500, error: localeMessages.internalServerError })
+
+    //     }
+    // })
 }
