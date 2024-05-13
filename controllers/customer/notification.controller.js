390efdf95c092dfbe6a7f1b7d6afded5a3ac0e5b
Abrahim Saad <abrahimsaad271@gmail.com>
2024-05-13 19:20:54 +0300

    Notification Module

--- controllers/customer/notification.controller.js
+++ controllers/customer/notification.controller.js
@@ -4,10 +4,16 @@ const notificationRepo = require("../../modules/Notification/notification.repo")
 
 exports.listNotifications = async (req, res) => {
     try {
-        const filterObject = req.query;
+        let filterObject = req.query;
         const pageNumber = req.query.page || 1, limitNumber = req.query.limit || 10
         if (filterObject.seenBy == "false") filterObject.seenBy = { $nin: [req.tokenData._id] }
 
+        filterObject.$or = [
+            { toAll: true },
+            { toAllCustomers: true },
+            { receivers: filterObject.receivers || req.tokenData._id }
+        ]
+        delete filterObject["receivers"]
         const operationResultObject = await notificationRepo.list(filterObject, { receivers: 0 }, { timestamp: -1 }, pageNumber, limitNumber);
         operationResultObject.result = notificationRepo.isSeenByUser(operationResultObject.result, req.tokenData._id)
         return res.status(operationResultObject.code).json(operationResultObject);
--- controllers/seller/notification.controller.js
+++ controllers/seller/notification.controller.js
@@ -7,7 +7,12 @@ exports.listNotifications = async (req, res) => {
         const filterObject = req.query;
         const pageNumber = req.query.page || 1, limitNumber = req.query.limit || 10
         if (filterObject.seenBy == "false") filterObject.seenBy = { $nin: [req.tokenData._id] }
-
+        filterObject.$or = [
+            { toAll: true },
+            { toAllSellers: true },
+            { receivers: filterObject.receivers || req.tokenData._id }
+        ]
+        delete filterObject["receivers"]
         const operationResultObject = await notificationRepo.list(filterObject, { receivers: 0 }, { timestamp: -1 }, pageNumber, limitNumber);
         operationResultObject.result = notificationRepo.isSeenByUser(operationResultObject.result, req.tokenData._id)
         return res.status(operationResultObject.code).json(operationResultObject);
