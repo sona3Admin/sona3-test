26c16c8f8db717509f8b1ef3c936bc1410107a57
Abrahim Saad <abrahimsaad271@gmail.com>
2024-06-11 09:05:35 +0300

    Cancel Orders

--- controllers/customer/order/basket.controller.js
+++ controllers/customer/order/basket.controller.js
@@ -3,7 +3,7 @@ const orderRepo = require("../../../modules/Order/order.repo")
 const basketRepo = require("../../../modules/Basket/basket.repo");
 const { handleOrderCreation, handleReverseOrderCreation } = require("../../../helpers/order.helper")
 const ifastShipperHelper = require("../../../utils/ifastShipping.util")
-
+const { findObjectInArray } = require("../../../helpers/cart.helper")
 
 exports.createOrder = async (req, res) => {
     try {
@@ -65,15 +65,15 @@ exports.returnSubOrder = async (req, res) => {
 exports.cancelSubOrder = async (req, res) => {
     try {
         let orderObject = await orderRepo.get({ _id: req.query._id, "subOrders._id": req.query.subOrder })
-        
+
         let subOrderObject = findObjectInArray(orderObject.result.subOrders, "_id", req.query.subOrder)
         if (!subOrderObject.success) return res.status(404).json({ success: false, code: 404, error: i18n.__("notFound") });
 
-
         let operationResultObject = await ifastShipperHelper.cancelOrderShipment(subOrderObject.result.shippingId)
-
         if (!operationResultObject.success) return res.status(500).json({ success: false, code: 500, error: i18n.__("internalServerError") });
-
+        let subOrders = orderObject.result.subOrders
+        subOrders[subOrderObject.index].status = "canceled"
+        operationResultObject = await orderRepo.updateDirectly(orderObject.result._id.toString(), { subOrders })
         return res.status(operationResultObject.code).json(operationResultObject);
 
     }
