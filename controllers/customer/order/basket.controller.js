3611d860009ee8b54854376d4f4da58d397c7820
Abrahim Saad <abrahimsaad271@gmail.com>
2024-06-11 07:43:08 +0300

    Return Orders

--- controllers/customer/order/basket.controller.js
+++ controllers/customer/order/basket.controller.js
@@ -60,3 +60,29 @@ exports.returnSubOrder = async (req, res) => {
         });
     }
 }
+
+
+exports.cancelSubOrder = async (req, res) => {
+    try {
+        let orderObject = await orderRepo.get({ _id: req.query._id, "subOrders._id": req.query.subOrder })
+        
+        let subOrderObject = findObjectInArray(orderObject.result.subOrders, "_id", req.query.subOrder)
+        if (!subOrderObject.success) return res.status(404).json({ success: false, code: 404, error: i18n.__("notFound") });
+
+
+        let operationResultObject = await ifastShipperHelper.cancelOrderShipment(subOrderObject.result.shippingId)
+
+        if (!operationResultObject.success) return res.status(500).json({ success: false, code: 500, error: i18n.__("internalServerError") });
+
+        return res.status(operationResultObject.code).json(operationResultObject);
+
+    }
+    catch (err) {
+        console.log(`err.message controller`, err.message);
+        return res.status(500).json({
+            success: false,
+            code: 500,
+            error: i18n.__("internalServerError")
+        });
+    }
+}
