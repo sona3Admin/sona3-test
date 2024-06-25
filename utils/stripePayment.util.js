b2aaf419fe6bcf93534bf4d9e489c3a6f8d4b2bc
Abrahim Saad <abrahimsaad271@gmail.com>
2024-06-25 09:35:45 +0300

    ifast

--- utils/stripePayment.util.js
+++ utils/stripePayment.util.js
@@ -1,29 +1,82 @@
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
 
-module.exports = async (req, res) => {
+exports.initiatePayment = async (orderCostObject, orderDetails) => {
     try {
+
+        const clientType = req.body.clientType;
+        let successUrl = `${process.env.STRIPE_SUCCESS_URL}`;
+        let cancelUrl = `${process.env.STRIPE_CANCEL_URL}`
+
+        if (clientType === 'mobile') {
+            successUrl = `${process.env.STRIPE_SUCCESS_DEEP_LINK}`
+            cancelUrl = `${process.env.STRIPE_CANCEL_DEEP_LINK}`
+        }
+
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             mode: "payment",
-            line_items: req.body.items.map(item => {
-                const storeItem = storeItems.get(item.id)
-                return {
+            line_items: [
+                {
                     price_data: {
-                        currency: "usd",
+                        currency: "aed",
                         product_data: {
-                            name: storeItem.name,
+                            name: "Items Total",
                         },
-                        unit_amount: storeItem.priceInCents,
+                        unit_amount: orderCostObject.cartTotal,
                     },
-                    quantity: item.quantity,
+                    quantity: 1,
+                },
+                {
+                    price_data: {
+                        currency: "aed",
+                        product_data: {
+                            name: "Tax",
+                        },
+                        unit_amount: orderCostObject.taxesTotal,
+                    },
+                    quantity: 1,
+                },
+                {
+                    price_data: {
+                        currency: "aed",
+                        product_data: {
+                            name: "Shipping Fee",
+                        },
+                        unit_amount: orderCostObject.shippingFeesTotal,
+                    },
+                    quantity: 1,
                 }
-            }),
-            success_url: `${process.env.CLIENT_URL}/success.html`,
-            cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+            ],
+            success_url: successUrl,
+            cancel_url: cancelUrl,
+            metadata: { ...orderDetails }
         })
-        res.json({ url: session.url })
-    } catch (e) {
-        res.status(500).json({ error: e.message })
+        return { success: true, code: 201, result: session.url }
+    } catch (err) {
+        console.log("err", err.message)
+        return { success: false, code: 500, error: err.message }
     }
+}
+
+
+exports.getPaymentSuccessAck = (req, res, next) => {
+    try {
+        const sig = req.headers['stripe-signature'];
+        let event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        if (event.type === 'checkout.session.completed') {
+            const session = event.data.object;
+            const orderDetails = session.metadata;
+            req.body = orderDetails
+            console.log("orderDetails", orderDetails)
+            return next()
+        }
+
+    } catch (err) {
+        console.log("err.message", err.message)
+        return res.status(400).json({ success: false, code: 400, error: err.message })
+    }
+
+
+
 }
