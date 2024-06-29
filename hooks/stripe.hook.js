862ec2a084e39807c1e42e81de77922598a3c8ce
Abrahim Saad <abrahimsaad271@gmail.com>
2024-06-29 08:20:56 +0300

    stripe

--- hooks/stripe.hook.js
+++ hooks/stripe.hook.js
@@ -6,7 +6,8 @@ exports.getPaymentSuccessAck = (req, res, next) => {
         console.log("triggered")
         console.log("req.body", req.body)
         const sig = req.headers['stripe-signature'];
-        let event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        const rawBody = req.body.toString('utf8');
+        let event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
         // if (event.type === 'checkout.session.completed' || event.type == "payment_intent.succeeded") {
         const session = event.data.object;
         // const orderDetails = session.metadata;
