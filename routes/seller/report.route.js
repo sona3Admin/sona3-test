b7f21b4bfc19f13d349d0c6eb53c76ed9a6d6a35
Hozifaelgharbawy <hozifaelgharbawy@gmail.com>
2025-02-03 14:34:24 +0200

    feat: add new report endpoints for orders and service requests statistics

--- routes/seller/report.route.js
+++ routes/seller/report.route.js
@@ -7,6 +7,13 @@ app.get("/services", reportController.countServices);
 
 
 
+app.get("/ordersByDay", reportController.getOrdersStatsByDay);
+app.get("/ordersByMonth", reportController.getOrdersStatsByMonth);
+app.get("/requestsByDay", reportController.getServiceRequestsStatsByDay);
+app.get("/requestsByMonth", reportController.getServiceRequestStatsByMonth);
+app.get("/financesOrders", reportController.financesOrders);
+app.get("/financesServicesRequest", reportController.financesServicesRequest);
+
 
 app.get("/order", reportController.countOrders);
 app.get("/request", reportController.countRequests);
